163d783efe0e5cc84d7f8b302a8fd6ad4b80d7d7
katrin <kkopeiko@mail.ru>
2020-04-13 20:30:54 +0300

    first commit

---
+++ IDataSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExercises
+{
+    public interface IDataSource
+    {
+        List<Product> Products { get;  }
+        List<Supplier> Suppliers { get; }
+        List<Customer> Customers { get; }
+    }
+}
